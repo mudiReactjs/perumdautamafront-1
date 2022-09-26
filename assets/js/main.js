1c10b0e721047e7b27be8f10674a7cc1ef6a29ac
andris0405 <kuliah0405@gmail.com>
2022-09-26 20:45:24 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// make function scroll down navbar change backround color
+const navbar = document.querySelector('.navbar');
+window.addEventListener('scroll', (event) => {
+    const scrollY = window.scrollY;
+    if (scrollY > 20) {
+        navbar.classList.remove('bg-dark-rgba');
+        navbar.classList.add('bg-dark');
+    } else {
+        navbar.classList.add('bg-dark-rgba');
+        navbar.classList.remove('bg-dark');
+    }
+});
+
+// Initialize evo-calendar
+$(document).ready(function () {
+    $('#calendar').evoCalendar({
+        language: 'id',
+        format: 'dd/mm/yyyy',
+        sidebarDisplayDefault: false,
+    })
+})
+
+// const navbarToggler = document.querySelector('.navbar-toggler');
+// navbarToggler.addEventListener('click', (event) => {
+//     navbar.classList.remove('bg-dark-rgba');
+//     navbar.classList.add('bg-dark');
+//     console.log('anjat');
+// });
